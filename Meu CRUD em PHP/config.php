6dbb600cbbad61e74c5bac58ddb57f1e34da5b78
Daniel Ramos <90581303+drammistein@users.noreply.github.com>
2021-10-08 10:52:21 -0300

    Update config.php

--- Meu CRUD em PHP/config.php
+++ Meu CRUD em PHP/config.php
@@ -2,7 +2,7 @@
 $db_name = 'grupo';
 $db_host = 'localhost';
 $db_user = 'root';
-$db_pass = 'Jesus1am#*';
+$db_pass = 'senha';
 
     $pdo = new PDO("mysql:dbname=".$db_name.";host=".$db_host, $db_user, $db_pass);
 ?>
